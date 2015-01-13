684e79baae504244a633ae472282e90fe32ff4f1
TJF <Thomas.Freiherr@gmx.net>
2015-01-13 12:48:50 +0100

    naming Fbc implicit libs and dirs out-of-source build addaptions minor optimization
    Signed-off-by: TJF <Thomas.Freiherr@gmx.net>

--- CMakeDetermineFbcCompiler.cmake
+++ CMakeDetermineFbcCompiler.cmake
@@ -1,7 +1,7 @@
 #
 # CMakeFbc - CMake module for FreeBASIC Language
 #
-# Copyright (C) 2014, Thomas{ dOt ]Freiherr[ aT ]gmx[ DoT }net
+# Copyright (C) 2014-2015, Thomas{ dOt ]Freiherr[ aT ]gmx[ DoT }net
 #
 # All rights reserved.
 #
@@ -11,12 +11,11 @@
 # See http://www.cmake.org/HTML/Copyright.html for details
 #
 
-# determine the compiler to use for FreeBASIC programs
-# NOTE, a generator may set CMAKE_Fbc_COMPILER before
-# loading this file to force a compiler.
-# use environment variable FBC first if defined by user, next use
-# the cmake variable CMAKE_GENERATOR_FBC which can be defined by a generator
-# as a default compiler
+# determine the compiler to use for FreeBASIC programs NOTE, a generator
+# may set CMAKE_Fbc_COMPILER before loading this file to force a
+# compiler. use environment variable FBC first if defined by user, next
+# use the cmake variable CMAKE_GENERATOR_Fbc which can be defined by a
+# generator as a default compiler
 IF(NOT CMAKE_Fbc_COMPILER)
   # prefer the environment variable FBC
   IF($ENV{FBC} MATCHES ".+")
@@ -31,9 +30,9 @@ IF(NOT CMAKE_Fbc_COMPILER)
   ENDIF()
 
   # next try prefer the compiler specified by the generator
-  IF(CMAKE_GENERATOR_FBC)
+  IF(CMAKE_GENERATOR_Fbc)
     IF(NOT CMAKE_Fbc_COMPILER_INIT)
-      SET(CMAKE_Fbc_COMPILER_INIT ${CMAKE_GENERATOR_FBC})
+      SET(CMAKE_Fbc_COMPILER_INIT ${CMAKE_GENERATOR_Fbc})
     ENDIF()
   ENDIF()
 
@@ -50,8 +49,15 @@ IF(NOT CMAKE_Fbc_COMPILER)
     SET(CMAKE_Fbc_COMPILER "${CMAKE_Fbc_COMPILER_INIT}" CACHE FILEPATH "fbc compiler" FORCE)
   ENDIF()
 ENDIF()
-MARK_AS_ADVANCED(CMAKE_Fbc_COMPILER)
 GET_FILENAME_COMPONENT(COMPILER_LOCATION "${CMAKE_Fbc_COMPILER}" PATH)
+
+IF(NOT CMAKE_Fbc_DEPS_TOOL)
+  FIND_PROGRAM(CMAKE_Fbc_DEPS_TOOL cmake_fb_deps DOC "FreeBASIC dependencies tool.")
+  IF(CMAKE_Fbc_DEPS_TOOL)
+    SET(CMAKE_Fbc_DEPS_TOOL "${CMAKE_Fbc_DEPS_TOOL}" CACHE FILEPATH "cmake FB dependency tool" FORCE)
+    MESSAGE(STATUS "Check for working cmake_fb_deps tool OK ==> ${CMAKE_Fbc_DEPS_TOOL}")
+  ENDIF()
+ENDIF()
 
 FIND_PROGRAM(CMAKE_AR NAMES ar PATHS ${COMPILER_LOCATION} )
 
@@ -59,7 +65,6 @@ FIND_PROGRAM(CMAKE_RANLIB NAMES ranlib)
 IF(NOT CMAKE_RANLIB)
    SET(CMAKE_RANLIB : CACHE INTERNAL "noop for ranlib")
 ENDIF()
-MARK_AS_ADVANCED(CMAKE_RANLIB)
 
 SET(CMAKE_COMPILER_IS_FBC 1)
 FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
@@ -71,10 +76,14 @@ IF(NOT CMAKE_PLATFORM_INFO_DIR)
 ENDIF()
 
 # configure variables set in this file for fast reload later on
-CONFIGURE_FILE(${CMAKE_ROOT}/Modules/CMakeFbcCompiler.cmake.in
+CONFIGURE_FILE(${CMAKE_MODULE_PATH}/CMakeFbcCompiler.cmake.in
   ${CMAKE_PLATFORM_INFO_DIR}/CMakeFbcCompiler.cmake
   @ONLY IMMEDIATE # IMMEDIATE must be here for compatibility mode <= 2.0
   )
 
-MARK_AS_ADVANCED(CMAKE_AR)
+MARK_AS_ADVANCED(
+  CMAKE_AR
+  CMAKE_RANLIB
+  CMAKE_Fbc_COMPILER
+  )
 SET(CMAKE_Fbc_COMPILER_ENV_VAR "FBC")
