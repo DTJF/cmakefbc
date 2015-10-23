eb308330c8c41843ea2aadd3091ef58eef98abd8
TJF <Thomas.Freiherr@gmx.net>
2015-10-23 05:44:29 +0200

    path in determine again
    Signed-off-by: TJF <Thomas.Freiherr@gmx.net>

--- cmake/Modules/CMakeDetermineFbcCompiler.cmake
+++ cmake/Modules/CMakeDetermineFbcCompiler.cmake
@@ -71,16 +71,21 @@ FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
   "Determining fbc compiler as ${CMAKE_Fbc_COMPILER}\n\n")
 
 # fix for CMake < 2.8.10
-IF(NOT CMAKE_PLATFORM_INFO_DIR)
-  SET(CMAKE_PLATFORM_INFO_DIR ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY})
+IF(CMAKE_PLATFORM_INFO_DIR)
+  SET(pdir ${CMAKE_PLATFORM_INFO_DIR})
+ELSE
+  SET(pdir ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY})
 ENDIF()
-IF(NOT CMAKE_MODULE_PATH)
-  SET(CMAKE_MODULE_PATH ${CMAKE_ROOT}/Modules/)
+IF(CMAKE_MODULE_PATH)
+  SET(mdir ${CMAKE_MODULE_PATH}
+ELSE
+  SET(mdir ${CMAKE_ROOT}/Modules/)
 ENDIF()
 
 # configure variables set in this file for fast reload later on
-CONFIGURE_FILE(${CMAKE_MODULE_PATH}CMakeFbcCompiler.cmake.in
-  ${CMAKE_PLATFORM_INFO_DIR}/CMakeFbcCompiler.cmake
+CONFIGURE_FILE(
+  ${mdir}/CMakeFbcCompiler.cmake.in
+  ${pdir}/CMakeFbcCompiler.cmake
   @ONLY IMMEDIATE # IMMEDIATE must be here for compatibility mode <= 2.0
   )
 
